41125149b1e73fb6ca627eedd985d9a9e5b365f9
mecenefeaa <mecenefeaa@gmail.com>
2012-11-24 14:07:38 -0500

    updated build script to place new pack file in the .app, this allows people who have ioquake3 to run both from the same directory

--- osx.sh
+++ osx.sh
@@ -8,8 +8,8 @@ cd build/release-darwin-i386/baseq3
 
 zip pak9.pk3 vm/*
 
-mv pak9.pk3 ../../../../tests/baseq3
+mv pak9.pk3 ../../release-darwin-x86_64/spearmint.app/Contents/MacOS/baseq3
 
 cd ../../release-darwin-x86_64/
 
-cp -a spearmint.app ../../../tests/spearmint.app
+cp -a spearmint.app ../../../tests/
