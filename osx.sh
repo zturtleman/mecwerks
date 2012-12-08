254ee59f52b5b60c7323aa9e3221b78001828c08
mecenefeaa <mecenefeaa@gmail.com>
2012-12-07 19:37:47 -0500

    updated osx.sh script

--- osx.sh
+++ osx.sh
@@ -8,8 +8,8 @@ cd build/release-darwin-i386/baseq3
 
 zip pak9.pk3 vm/*
 
-mv pak9.pk3 ../../release-darwin-x86_64/spearmint.app/Contents/MacOS/baseq3
+mv pak9.pk3 ../../release-darwin-x86_64/mecwerks.app/Contents/MacOS/baseq3
 
 cd ../../release-darwin-x86_64/
 
-cp -a spearmint.app ../../../tests/
+cp -a mecwerks.app ../../../tests/
