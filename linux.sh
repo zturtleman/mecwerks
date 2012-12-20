0c3cba53ca2faf45e7f97e9dada8ae0ce600f078
Marcus <mecenefeaa@gmail.com>
2012-12-19 19:02:37 -0500

    edited build script to add botfiles to pak9.pk3

--- linux.sh
+++ linux.sh
@@ -4,10 +4,16 @@ make
 
 cd build/release-linux-x86_64/baseq3
 
-zip pak9.pk3 vm/*
+zip -q pak9.pk3 vm/*
 
-mv pak9.pk3 ../../../../tests/baseq3
+mv pak9.pk3 ../../../code/
 
 cd ../
 
 cp * ../../../tests/
+
+cd ../../code/
+
+zip -rqu pak9.pk3 botfiles/
+
+mv pak9.pk3 ../../tests/baseq3
